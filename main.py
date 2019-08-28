03d74d4819c2ba7bb9c473ecee5d8aca8ed66022
twoldstad <thwoldstad@gmail.com>
2019-08-28 16:45:38 -0700

    added polish

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import os.path
 import csv
 import re
 import textwrap
@@ -85,22 +86,29 @@ def input_message(doc):
    return fle
 
 def imf_input():
-   doc = '1.csv' ###########################################input_message('IMF')
+   doc = input_message('IMF')
+   if doc == 'demo':
+      doc = os.path.join('sample', 'sample_imf.csv')
    with open(doc) as file_:
       csv_ = csv.reader(file_, delimiter=',')
       skip = next(csv_)
       for line in csv_:
          line = [e.strip() for e in line]
          prt = part(str(line[0]), int(line[1]), int(line[2]), int(line[3]), int(line[4]), int(line[5]))
-         sr = re.split('[: ]', line[6])
-         sr = [int(e) for e in sr]
-         prt.sr = dict(zip(sr[0::2], sr[1::2]))
+         try:
+            sr = re.split('[: ]', line[6])
+            sr = [int(e) for e in sr]
+            prt.sr = dict(zip(sr[0::2], sr[1::2]))
+         except ValueError:
+            pass
          all_parts.append(prt)
          part_names[prt.name] = prt
 
 def mps_input():
    global num_periods
-   doc = '2.csv' ############################################input_message('MPS')
+   doc = input_message('MPS')
+   if doc == 'demo':
+      doc = os.path.join('sample', 'sample_mps.csv')
    with open(doc) as file_:
       csv_ = csv.reader(file_, delimiter=',')
       header = next(csv_)
@@ -116,7 +124,9 @@ def mps_input():
          prt.mps = dict(zip(periods, [int(e) for e in line[1:]]))
 
 def bom_input():
-   doc = '3.csv' #############################################input_message('BOM')
+   doc = input_message('BOM')
+   if doc == 'demo':
+      doc = os.path.join('sample', 'sample_bom.csv')
    with open(doc) as file_:
       csv_ = csv.reader(file_, delimiter=',')
       skip = next(csv_)
@@ -166,40 +176,42 @@ def solver():
          need_lst = [0] * (num_periods + neg)
          release_lst = need_lst[::]
          avail_lst = need_lst[::]
-         avail = current_part.oh - current_part.alloc
+         avail = 0
+         first_add = False
          # go through demand for part and add to list that accounts for negative periods
          for period, qt in current_part.mps.items():
             need_lst[int(period) + neg - 1] = qt
          # go through list of demand for part while maintaining correct period references
          for per, qt in enumerate(need_lst, start=(1-neg)):
-            i_per = per + neg -1
+            i_per = per + neg - 1
+            avail += current_part.sr.get(per, 0)
+            if per > 0 and first_add is False:
+                  avail += current_part.oh - current_part.alloc
+                  first_add = True
             if qt > 0:
-               if 1 - (per-current_part.lt) > neg:
-                  new_neg = 1 - (per-current_part.lt)
-                  neg_dif = new_neg - neg
-                  release_lst = [0]*neg_dif + release_lst
-                  need_lst = [0]*neg_dif + need_lst
-                  avail_lst = [0]*neg_dif + avail_lst
-                  i_per += neg_dif
-                  neg = new_neg
-               if per > 0:
-                  avail += current_part.sr.get(per, 0)
-                  if avail - need_lst[i_per] < current_part.ss:
-                     short = need_lst[i_per] - avail
-                     buy = (short // current_part.ls + (short % current_part.ls)) * current_part.ls
-                     when = per - current_part.lt
-                     i_when = when + neg - 1
-                     release_lst[i_when] += buy
-                     avail += buy
-                     for child, amount in current_part.bom.items():
-                        child.mps[when] = amount*buy + child.mps.get(when, 0)
-               else:
+               leftover = avail - qt
+               if leftover < current_part.ss:
                   when = per - current_part.lt
-                  i_when = when + neg - 1
-                  release_lst[i_when] = qt
+                  i_when = when + neg - 1  
+                  if 1 - when > neg:
+                     new_neg = 1 - when
+                     neg_dif = new_neg - neg
+                     release_lst = [0]*neg_dif + release_lst
+                     need_lst = [0]*neg_dif + need_lst
+                     avail_lst = [0]*neg_dif + avail_lst
+                     i_per += neg_dif
+                     i_when += neg_dif
+                     neg = new_neg
+                  if per > 0:
+                     short = current_part.ss - leftover
+                     buy = (short // current_part.ls + (short % current_part.ls > 0)) * current_part.ls
+                  else:
+                     buy = qt
+                  avail += buy
+                  release_lst[i_when] += buy
                   for child, amount in current_part.bom.items():
-                     child.mps[when] = amount*buy + child.mps[when]
-            avail -= need_lst[i_per]
+                     child.mps[when] = amount*buy + child.mps.get(when, 0)
+               avail -= qt
             avail_lst[i_per] = avail
          current_part.mrp = release_lst
          current_part.avl = avail_lst
@@ -212,7 +224,7 @@ space = len(subtitle) + 2
 opening = 'In order to calculate a material resource planning (MRP) order-release schedule, an item master file (IMF), manufacturing production schedule (MPS), and bill of materials (BOM) is required. Please check the example files provided for correct formatting. NOTE: This program is designed to account for available materials from the present time period. A correct MPS should not incur past-due or late orders. However, this program will still consider such orders and will report them without applying presently available resources. As such, material quantities needed in the past will be reported in exact quantities without regard to lot sizes and will not impact future available quantities.'
 b_table_title = 'Indented BOM'
 a_table_title = 'Available Quantities Per Time Period'
-m_table_title = 'MRP Schedule'
+m_table_title = 'MRP Order-Release Schedule'
 print((title.center(space)).center(100, '#')+'\n')
 print(subtitle.center(100)+'\n\n')
 print(textwrap.fill(opening, 100)+'\n')
